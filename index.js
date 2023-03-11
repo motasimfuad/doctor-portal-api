e2aac9dd4b601d8d456e4046223530edadde92ed
motasimfuad <motasimfuad0@gmail.com>
2023-03-11 16:52:53 +0600

    index and package is updated

--- index.js
+++ index.js
@@ -1,33 +1,167 @@
-// setup
-import cors from 'cors';
-import express from 'express';
-const port = process.env.port || 5000
-
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+​
+require("dotenv").config();
+​
 const app = express();
-// middleware 
+const port = process.env.PORT || 5000;
+​
+//middle ware
+​
 app.use(cors());
 app.use(express.json());
-
-
-//api route
-
-app.get('/', (req,res)=>{
-    res.send('Hello world')
-})
-
-app.use((req, res, next) => {
-    next(createError.NotFound());
-  });
-  
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send({
-      status: err.status || 500,
-      message: err.message,
+​
+//database info
+//name: conceptual2
+//pass: HiYzi4UJ1qHP7sHn
+​
+//collection name
+//name: phoneData
+// data name: phone
+​
+//data base connection
+​
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.nvnfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+​
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+​
+//for order
+​
+async function orders() {
+  try {
+    await client.connect();
+    const orderCollection = client.db("phoneData").collection("orders");
+​
+    //post
+    app.post("/order", async (req, res) => {
+      const orders = req.body;
+      const result = await orderCollection.insertOne(orders);
+      res.send(result);
     });
-  });
+  } finally {
+    // await client.close();
+  }
+}
+​
+orders().catch(console.dir);
+​
+async function run() {
+  try {
+    await client.connect();
+    const phoneCollection = client.db("phoneData").collection("phone");
+​
+    //add order
+​
+  /*   app.post("/order", async (req, res) => {
+      const orders = req.body;
+      console.log(orders);
+      const result = await phoneCollection.insertOne(orders);
+      res.send(result);
+    }); */
+​
+    //post api
+​
+    app.post("/books", async (req, res) => {
+      const book = req.body;
+      if (!book?.name || !book?.img || !book?.Price) {
+        return res.send({
+          success: false,
+          error: "please fillup all require",
+        });
+      }
+      const result = await phoneCollection.insertOne(book);
+      res.send({
+        success: true,
+        message: `successfully added ${book.name}`,
+      });
+    });
+​
+    //get api
+    app.get("/books", async (req, res) => {
+      const page = Number(req.query.page);
+      const size = Number(req.query.size);
+      console.log(page, size);
+      const querry = {};
+      const cursor = phoneCollection.find(querry);
+      const result = await cursor
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send({ success: true, data: result });
+    });
+​
+    //get count
+    app.get("/productCount", async (req, res) => {
+      const query = {};
+      const curser = phoneCollection.find(query);
+      const count = await curser.count();
+      res.send({ count });
+    });
+​
+    //delte
+​
+    app.delete("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const querry = { _id: ObjectId(id) };
+      const result = await phoneCollection.deleteOne(querry);
+      res.send(result);
+    });
+​
+    //update
+​
+    app.get("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await phoneCollection.findOne(query);
+      res.send(result);
+    });
+​
+    //put
+​
+    app.put("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updateUser.name,
+          seller: updateUser.seller,
+          discription: updateUser.discription,
+          Price: updateUser.Price,
+          quantity: updateUser.quantity,
+          img: updateUser.img,
+        },
+      };
+      const result = await phoneCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+​
+    //update
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+​
+//cheek endpoint
+​
+app.get("/", (req, res) => {
+  res.send({ message: "server is running" });
+});
+​
+app.listen(port, () => {
+  console.log("server running on port", port);
+});
 
-
-app.listen(port,()=>{
-    console.log('app is running', port);
-})
+//docker build -t myapp .
+//docker run -p 5000:5000 myapp
